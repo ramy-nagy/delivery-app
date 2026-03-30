b765155062528046a2f984292e1a15a9a6b62d02
ramynagy <ramimhmoud2020@gmail.com>
2026-03-30 21:48:29 +0200

    add fcm report stocks node

--- app/Http/Controllers/Api/V1/MainCategoryController.php
+++ app/Http/Controllers/Api/V1/MainCategoryController.php
@@ -4,19 +4,22 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\MenuCategorySingleResource;
+use App\Http\Traits\ApiResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\MenuCategory;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class MainCategoryController extends Controller
 {
-    public function index(): AnonymousResourceCollection
+    use ApiResponse;
+    public function index()
     {
         $categories = MenuCategory::orderBy('sort_order')->orderBy('name')->get();
-        return MenuCategorySingleResource::collection($categories);
+        return $this->success(MenuCategorySingleResource::collection($categories), 'Main categories fetched successfully.');
     }
 
-    public function show(MenuCategory $menuCategory): MenuCategorySingleResource
+    public function show(MenuCategory $menuCategory)
     {
-        return new MenuCategorySingleResource($menuCategory->load('items'));
+        return $this->success(new MenuCategorySingleResource($menuCategory->load('items')), 'Main category fetched successfully.');
     }
 }
